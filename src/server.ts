58a2a1508c7cea7b92619aa8cd3a6dbff1945fdd
byell-spec <gabryel.lymabiel@gmail.com>
2021-08-24 13:19:48 -0300

    second class

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,14 @@
+import "reflect-metadata";
 import express from "express";
+
+import { router } from './routes';
+
+import "./database";
 
 const app = express();
 
-/**
- * GET => Search a information
- * POST => Create a information
- * PUT => Change a information
- * DELETE => Delete a information
- * PATCH => Change a specific information
- */
+app.use(express.json());
 
-app.get("/test", (request, response) => {
-  // Request => Entering
-  // Respose => Leaving
-  return response.send("Olá NLW");
-})
-
-app.post("/test-post", (request, response) => {
-  return response.send("Olá NLW POST");
-})
+app.use(router);
 
 app.listen(3000, () => console.log("Server is running"));
